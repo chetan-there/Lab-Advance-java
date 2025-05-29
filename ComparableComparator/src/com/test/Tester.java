f9c866646c96b2e5c251dd3f5442cc2752ed460f
chetan-there <therechetan24@gmail.com>
2025-05-29 11:58:11 +0530

    shoe added

--- ComparableComparator/src/com/test/Tester.java
+++ ComparableComparator/src/com/test/Tester.java
@@ -9,7 +9,7 @@ public class Tester {
 
         ArrayList<Product> products = new ArrayList<>();
         products.add(new Product(101, "Pen", 100));
-        products.add(new Product(102, "Pencil", 60));
+        products.add(new Product(102, "Shoe", 60));
         products.add(new Product(103, "book", 200));
 
         Collections.sort(products);
